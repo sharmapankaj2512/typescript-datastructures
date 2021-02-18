e89cb19ad02494b62d8d77eba405f4b4b32ab349
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2021-02-18 12:17:02 +0400

    Used hashmap for mapping customer type to rate

--- src/__tests__/cheapest-hotel.test.ts
+++ src/__tests__/cheapest-hotel.test.ts
@@ -24,7 +24,7 @@ test('should return Bridgewood for regular customer for weekdays and weekends', 
 test('should return Ridgewood for rewards customer on weekdays and weekends', () => {
     //given
     let customerType = CustomerType.Rewards;
-    let travelDates = [new Date('2009-03-26'), new Date('2009-03-27'), new Date('2009-03-28')]
+    let travelDates = [new Date('2009-03-26'), new Date('2009-03-27'), new Date('2009-03-28')];
     //when
 
     //then
@@ -44,6 +44,9 @@ class Hotel {
     rewards_weekday: number;
     regular_weekend: number;
     rewards_weekend: number;
+    regularMap: Map<number, number>;
+    rewardsMap: Map<number, number>;
+    days_Map: Map<CustomerType, Map<number, number>>;
     rating: number;
     constructor(name: string, rating: number, [...rates]) {
         let [regular_weekday, rewards_weekday, regular_weekend, rewards_weekend] = [...rates];
@@ -53,56 +56,44 @@ class Hotel {
         this.rewards_weekday = rewards_weekday;
         this.regular_weekend = regular_weekend;
         this.rewards_weekend = rewards_weekend;
+        this.regularMap = new Map([
+            [0, regular_weekend],
+            [1, regular_weekday],
+            [2, regular_weekday],
+            [3, regular_weekday],
+            [4, regular_weekday],
+            [5, regular_weekday],
+            [6, regular_weekend]
+        ]);
+        this.rewardsMap = new Map([
+            [0, rewards_weekend],
+            [1, rewards_weekday],
+            [2, rewards_weekday],
+            [3, rewards_weekday],
+            [4, rewards_weekday],
+            [5, rewards_weekday],
+            [6, rewards_weekend]
+
+
+        ]);
+        this.days_Map = new Map([
+            [CustomerType.Regular, this.regularMap],
+            [CustomerType.Rewards, this.rewardsMap]
+        ]);
     }
 
     findTariff(customerType: CustomerType, travelDates: Array<Date>) {
-        if (customerType == CustomerType.Regular) {
-            let weekends = travelDates.filter(date => (date.getDay() == 0 || date.getDay() == 6));
-            let weekdays = travelDates.filter(date => (date.getDay() != 0 && date.getDay() != 6));
+        let sum = 0;
+        if (travelDates != null) {
+            travelDates.map(date => {
+                sum += this.days_Map.get(customerType).get(date.getDay());
+            });
 
-            let sum = 0;
-
-            if (weekends.length != 0) {
-                for (let i = 0; i < weekends.length; i++) {
-                    sum += this.regular_weekend;
-                }
-            }
-
-            if (weekdays.length != 0) {
-                for (let i = 0; i < weekdays.length; i++) {
-                    sum += this.regular_weekday;
-                }
-            }
-            console.log("hotel name:" + this.name + " " + sum)
             return sum;
-        }
-
-        if (customerType == CustomerType.Rewards) {
-
-            let weekends = travelDates.filter(date => (date.getDay() == 0 || date.getDay() == 6));
-            let weekdays = travelDates.filter(date => (date.getDay() != 0 && date.getDay() != 6));
-
-
-            let sum = 0;
-
-            if (weekends.length != 0) {
-
-                for (let i = 0; i < weekends.length; i++) {
-                    sum += this.rewards_weekend;
-
-                }
-            }
-
-            if (weekdays.length != 0) {
-                for (let i = 0; i < weekdays.length; i++) {
-                    sum += this.rewards_weekday;
-                }
-            }
-            console.log("hotel name:" + this.name + " " + sum)
-            return sum;
-
         }
         return -1;
+
+
     }
 }
 
@@ -115,8 +106,9 @@ function findCheapestHotel(customerType: CustomerType, travelDates: Array<Date>)
     let cheapestHotel = null;
     let cheapestTariff = 10000000;
 
+    
+
     hotels.forEach(hotel => {
-        console.log(hotel);
         let tariff = hotel.findTariff(customerType, travelDates);
         if (cheapestTariff > tariff) {
             cheapestTariff = tariff;
@@ -128,6 +120,7 @@ function findCheapestHotel(customerType: CustomerType, travelDates: Array<Date>)
             }
         }
     });
+
     return cheapestHotel.name;
 
 }
