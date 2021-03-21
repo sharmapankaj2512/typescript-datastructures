97582e34ea0cf3891d9dd7bde197ba38e5203789
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2021-03-21 17:55:34 +0400

    Refactor code

--- src/Doors.ts
+++ src/Doors.ts
@@ -1,16 +1,17 @@
 import Door from "./Door";
 
-export default class Doors{
-    doors:Array<Door>=[];
-    constructor(numberOfDoors:string) {
-        let no=parseInt(numberOfDoors);
-        if(isNaN(no)){
+export default class Doors {
+    doors: Array<Door> = [];
+
+    constructor(numberOfDoors: string) {
+        let doorsToCreate = parseInt(numberOfDoors);
+        if (isNaN(doorsToCreate)) {
             throw new Error("Please enter a number.");
-        }else if(no<0 || no>200){
-            throw new Error("Please enter number between 1 to 200.");
-        }else {
-            for (let i = 1; i <= no; i++)
-                this.doors.push(new Door(i, "CLOSED"));
         }
+        if (doorsToCreate < 0 || doorsToCreate > 200) {
+            throw new Error("Please enter number between 1 to 200.");
+        }
+        for (let i = 1; i <= doorsToCreate; i++)
+            this.doors.push(new Door(i, "CLOSED"));
     }
 }
